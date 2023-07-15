489fd40cac88c19e1e2b9b58551689adb8b32d26
cw-roy <curtis_roy@msn.com>
2023-07-15 07:42:36 -0700

    Update student controller and routes

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -1,7 +1,27 @@
 const studentController = {
-    findAll:  function (req, res) {
-        res.send("You have reached the controller method to return all Students")
-    }
-}
 
-module.exports = studentController;
+    findAll: function (req, res) {
+        res.send("You have reached the controller method to return all Students")
+    },
+    
+    findById: function (req, res) {
+        res.send("You asked for student " + req.params.id)
+    }
+  
+  }
+  
+  module.exports = studentController;
+
+// const studentController = {
+//     findAll:  function (req, res) {
+//         res.send("You have reached the controller method to return all Students")
+//     }
+// },
+
+//     findById: function (req, res) {
+//         res.send("You asked for student " + req.params.id)
+
+// }   
+
+
+// module.exports = studentController;
--- routes/student.routes.js
+++ routes/student.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const studentController = require("../controllers/student.controller")
 
 router.get('/students', studentController.findAll)
+router.get("/students/:id", studentController.findById)
 
 module.exports = router;
