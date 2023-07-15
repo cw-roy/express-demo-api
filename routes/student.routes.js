b40c792a046fc13efb6afcffe2ad36dbba55da3d
cw-roy <curtis_roy@msn.com>
2023-07-15 10:55:42 -0700

    Add ability to update JSON

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,9 +1,33 @@
+//require express and use it to create our router from express.Router()
 const express = require("express");
 const router = express.Router();
-
+  
 const studentController = require("../controllers/student.controller")
 
-router.get('/students', studentController.findAll)
+//get all the students
+router.get("/students", studentController.findAll)
+
+//get a student by id 
 router.get("/students/:id", studentController.findById)
 
+//add a student
+router.post("/students", studentController.add)
+
+
+//updatge a student by id 
+router.put("/students/:id", studentController.update)
+
+//updatge a student by id 
+router.delete("/students/:id", studentController.delete)
+
 module.exports = router;
+
+// const express = require("express");
+// const router = express.Router();
+
+// const studentController = require("../controllers/student.controller")
+
+// router.get('/students', studentController.findAll)
+// router.get("/students/:id", studentController.findById)
+
+// module.exports = router;
